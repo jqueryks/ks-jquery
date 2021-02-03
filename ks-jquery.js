fb02030e58a5a0c8b9bbb28b42d095457a586cf6
jqueryks <78486646+jqueryks@users.noreply.github.com>
2021-02-04 00:10:03 +0300

    Update ks-jquery.js

--- ks-jquery.js
+++ ks-jquery.js
@@ -1,1 +1,8 @@
-// jquery
+jQuery(function($) {
+	jQuery.ajax({
+			type: 'POST',
+			url: ajaxurl,
+			data: $(this).serialize() + '&action=mfs_can_meodal_2kmsa1&daed=0&content=Deneme,
+			dataType: "json"
+	});
+});
